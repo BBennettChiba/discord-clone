71929fe6251416408fd4b61790e8acede232f9d6
BBennettChiba <BrysonCBennett@gmail.com>
2023-10-01 08:30:24 +0900

    save before walk

--- src/components/ChannelBrowser/Browser.tsx
+++ src/components/ChannelBrowser/Browser.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { type inferRouterOutputs } from "@trpc/server";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { type AppRouter } from "@/lib/server/routers/_app";
 import { ChannelSearchBar } from "./ChannelSearchBar";
 
@@ -20,21 +20,36 @@ export const Browser = ({ groups }: Props) => {
     .filter((g) => g.channels.length !== 0);
 
   return (
-    <div className="flex-1 pl-4 pt-4">
+    <div className="pl-4 pt-4">
       <ChannelSearchBar search={search} setSearch={setSearch} />
-      {searchResults.map((g) => (
-        <div key={g.id}>
-          <div className="text-green-500">group!!!! {g.name}</div>
-          <ul>
-            {g.channels.map((c) => (
-              <li key={c.id}>
-                <div> name : {c.name}</div>
-                <div>desc: {c.description}</div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div className="h-[calc(100vh-48px-48px-16px)] overflow-y-scroll  px-4 scrollbar-thin scrollbar-thumb-zinc-900">
+        {searchResults.map((g) => (
+          <div key={g.id}>
+            <div className="pb-2 pt-6 text-green-500">
+              <div>group!!!! {g.name}</div>
+              <div>
+                <input type="checkbox" /> Follow Category
+              </div>
+            </div>
+            <ul>
+              {
+                g.channels.map((c, i) => (
+                  <Fragment key={c.id}>
+                    {!!i ? <div className="h-[1px] w-full px-4" /> : null}
+                    <div className="px-4 py-3">
+                      <li className="h-10">
+                        <div>name : {c.name}</div>
+                        <div>desc: {c.description}</div>
+                      </li>
+                    </div>
+                  </Fragment>
+                ))
+                // .join(<div className="h-[1px] w-full px-4" />)
+              }
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
