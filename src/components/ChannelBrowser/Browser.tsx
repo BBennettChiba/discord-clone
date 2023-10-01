c2040baaf15bb4df09d43b93486fd08d2ca2162c
BBennettChiba <BrysonCBennett@gmail.com>
2023-10-01 15:43:17 +0900

    styling buttons and checkboxes

--- src/components/ChannelBrowser/Browser.tsx
+++ src/components/ChannelBrowser/Browser.tsx
@@ -2,6 +2,7 @@
 import { type inferRouterOutputs } from "@trpc/server";
 import React, { Fragment, useState } from "react";
 import { type AppRouter } from "@/lib/server/routers/_app";
+import { Checkbox, Hash } from "../Icons";
 import { ChannelSearchBar } from "./ChannelSearchBar";
 
 type Props = {
@@ -24,28 +25,50 @@ export const Browser = ({ groups }: Props) => {
       <ChannelSearchBar search={search} setSearch={setSearch} />
       <div className="h-[calc(100vh-48px-48px-16px)] overflow-y-scroll  px-4 scrollbar-thin scrollbar-thumb-zinc-900">
         {searchResults.map((g) => (
-          <div key={g.id}>
-            <div className="pb-2 pt-6 text-green-500">
-              <div>group!!!! {g.name}</div>
-              <div>
-                <input type="checkbox" /> Follow Category
+          <div key={g.id} className="">
+            <div className="flex w-full justify-between pb-2 pt-6 text-xs">
+              <div>{g.name.toUpperCase()}</div>
+              <div className="mr-2 flex">
+                <div
+                  role="checkbox"
+                  className="mr-[6px] h-[14px] w-[14px] rounded-sm bg-indigo-500"
+                >
+                  <Checkbox className="h-[14px] w-[14px]" />
+                </div>
+                Follow Category
               </div>
             </div>
-            <ul>
-              {
-                g.channels.map((c, i) => (
-                  <Fragment key={c.id}>
-                    {!!i ? <div className="h-[1px] w-full px-4" /> : null}
-                    <div className="px-4 py-3">
-                      <li className="h-10">
-                        <div>name : {c.name}</div>
-                        <div>desc: {c.description}</div>
-                      </li>
+            <ul className="rounded-lg bg-zinc-700 bg-opacity-30">
+              {g.channels.map((c, i) => (
+                <Fragment key={c.id}>
+                  {!!i ? (
+                    <div className="px-4">
+                      <div className="h-[1px] w-full bg-gray-700" />
                     </div>
-                  </Fragment>
-                ))
-                // .join(<div className="h-[1px] w-full px-4" />)
-              }
+                  ) : null}
+                  <div className="group px-4 py-3">
+                    <li className="flex h-10 items-center justify-between">
+                      <div>
+                        <div className="flex items-center">
+                          <Hash className="h-5 w-5" />
+                          <div>&nbsp;{c.name}</div>
+                        </div>
+                        <div className="text-xs">{c.description}</div>
+                      </div>
+                      <div className="flex">
+                        <div className="pr-8">
+                          <button className="invisible border border-gray-500 rounded-sm text-sm px-4 py-1 group-hover:visible hover:bg-gray-500">
+                            View
+                          </button>
+                        </div>
+                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-indigo-500">
+                          <Checkbox className="h-[18px] w-[18px]" />
+                        </div>
+                      </div>
+                    </li>
+                  </div>
+                </Fragment>
+              ))}
             </ul>
           </div>
         ))}
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -76,3 +76,21 @@ export const X = ({ className }: Props) => (
     />
   </svg>
 );
+
+export const Checkbox = ({ className }: Props) => (
+  <svg
+    aria-hidden="true"
+    role="img"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    className={className}
+  >
+    <path
+      fill="white"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M8.99991 16.17L4.82991 12L3.40991 13.41L8.99991 19L20.9999 7.00003L19.5899 5.59003L8.99991 16.17Z"
+    />
+  </svg>
+);
