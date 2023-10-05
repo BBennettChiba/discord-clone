265d449910aa334d068756034ed97157cc3e732e
BBennettChiba <BrysonCBennett@gmail.com>
2023-10-05 12:30:47 +0900

    fetched only members of the channel

--- src/lib/server/routers/users.ts
+++ src/lib/server/routers/users.ts
@@ -1,9 +1,27 @@
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { db } from "@/lib/db";
+import { users } from "@/lib/db/schema/auth";
+import { usersToChannels } from "@/lib/db/schema/usersToChannels";
 import { publicProcedure, router } from "../trpc";
+
+const isNotNull = <T>(item: T | null): item is T => item !== null;
 
 export const usersRouter = router({
   getUsersByChannel: publicProcedure
     .input(z.object({ channelId: z.number() }))
-    .query(async () => await db.query.users.findMany()),
+    .query(async ({ input: { channelId } }) => {
+      const members = (
+        await db
+          .select({ user: users })
+          .from(usersToChannels)
+          .leftJoin(users, eq(users.id, usersToChannels.userId))
+          .where(eq(usersToChannels.channelId, channelId))
+      )
+        .map((m) => m.user)
+        .filter(isNotNull);
+      return members;
+    }),
 });
+
+/**@TODO  */
