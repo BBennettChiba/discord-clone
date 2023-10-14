3abf5eb5e29eb5031e8aba2f75d57532ad5c6698
BBennettChiba <BrysonCBennett@gmail.com>
2023-10-14 22:02:13 +0900

    undefined check in queries

--- src/lib/api/invites/queries.ts
+++ src/lib/api/invites/queries.ts
@@ -2,6 +2,7 @@ import { eq, sql } from "drizzle-orm";
 import { db } from "@/lib/db";
 import { invites } from "@/lib/db/schema/invites";
 import { usersToServers } from "@/lib/db/schema/usersToServers";
+import { throwError } from "@/lib/utils";
 
 type Input = {
   input: { id: string };
@@ -15,12 +16,16 @@ export const getInviteById = async ({ input: { id: inviteId } }: Input) => {
 
   if (!invite) throw new Error("No invite found");
 
-  const [{ numberOfMembers }] = await db
+  const [usersToServersQueryResult] = await db
     .select({
       numberOfMembers: sql<number>`count(*)`.mapWith(Number).as("count"),
     })
     .from(usersToServers)
     .where(eq(usersToServers.serverId, invite.server.id));
+
+  if (!usersToServersQueryResult)
+    return throwError("no usersToServersQueryResult found in getInviteById");
+  const { numberOfMembers } = usersToServersQueryResult;
 
   const inviteWithCount = {
     ...invite,
