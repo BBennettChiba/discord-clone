f2f3d1aa0f834262b36f1fe0e6ce7820ee987477
BBennettChiba <BrysonCBennett@gmail.com>
2023-10-20 17:06:12 +0900

    highlights user reactions

--- src/components/Channel/Reactions.tsx
+++ src/components/Channel/Reactions.tsx
@@ -1,6 +1,8 @@
 import { type EmojiMartData } from "@emoji-mart/data";
 import data from "@emoji-mart/data";
+import { useSession } from "next-auth/react";
 import { type CompleteMessage } from "@/lib/db/schema/messages";
+import { cn } from "@/lib/utils";
 
 type Props = {
   reactions: CompleteMessage["reactions"];
@@ -8,16 +10,30 @@ type Props = {
 
 export const Reactions = ({ reactions }: Props) => {
   const emojiData = (data as EmojiMartData).emojis;
+  
+  
+  const { data: session } = useSession();
+  if (!session)
+    throw new Error("something went wrong no session returned from useSession");
+  const userId = session.user.id;
 
   return (
-    <div className="flex h-[30px]">
+    <div className="flex gap-1">
       {reactions.map((r) => (
-        <div key={r.reactionId} className="flex">
-          <div className="text-xl">
-            {emojiData[r.reactionId]?.skins[0]?.native}
-          </div>
-          <div>{r.reactors.length}</div>
-        </div>
+        <button
+          key={r.reactionId}
+          className={cn(
+            "flex rounded-md border border-transparent bg-zinc-800 bg-opacity-30 px-[5px] py-[2px] hover:border-gray-500",
+            {
+              "border-indigo-500": r.reactors.some(
+                (reac) => reac.userId === userId,
+              ),
+            },
+          )}
+        >
+          <div className="">{emojiData[r.reactionId]?.skins[0]?.native}</div>
+          <div className="px-1">{r.reactors.length}</div>
+        </button>
       ))}
     </div>
   );
